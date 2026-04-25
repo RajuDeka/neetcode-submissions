99f74fbe153c372335609c2ab7e7579ef73d261e
RajuDeka <89827351+RajuDeka@users.noreply.github.com>
2026-04-25 15:14:00 +0530

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        count = [0] * 26
+        for c1,c2 in zip(s,t):
+            count[ord(c1) - ord('a')] += 1
+            count[ord(c2) - ord('a')] -= 1
+
+        return all(i == 0 for i in count)
+
