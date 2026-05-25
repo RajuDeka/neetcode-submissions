ade69c5bece270d0b3468560235f8630a16f8b9f
RajuDeka <89827351+RajuDeka@users.noreply.github.com>
2026-05-25 15:35:21 +0530

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        a = [0]*26
+        for c1,c2 in zip(s,t):
+            a[ord(c1)-ord('a')] += 1
+            a[ord(c2)-ord('a')] -= 1
+        return all (i == 0 for i in a)
